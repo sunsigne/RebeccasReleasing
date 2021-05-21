2dd0c904c95c4809c08a73be850275f685d70a93
Florian Murcia <florian.murcia@hotmail.fr>
2021-05-21 17:04:51 +0200

    10.3.2 (maeven removed)

--- RebeccasReleasing/src/com/sunsigne/rebeccasreleasing/toverify/system/util/MapCloneMaker.java
+++ RebeccasReleasing/src/com/sunsigne/rebeccasreleasing/toverify/system/util/MapCloneMaker.java
@@ -1,23 +1,13 @@
 package com.sunsigne.rebeccasreleasing.toverify.system.util;
 
-import java.lang.reflect.Type;
-import java.util.HashMap;
 import java.util.Map;
-
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
 public class MapCloneMaker {
 
 	public static <K, V> Map<K, V> clone(Map<K, V> map) {
 
-		Gson gson = new Gson();
-		String jsonString = gson.toJson(map);
-		 
-		Type type = new TypeToken<HashMap<K, V>>(){}.getType();
-		Map<K, V> clonedMap = gson.fromJson(jsonString, type); 
 
-		return clonedMap;
+		return map;
 	}
 
 }
